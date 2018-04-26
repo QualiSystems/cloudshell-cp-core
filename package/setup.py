a27485ef88da69894166e2f58ec86cf8fd0165ed
aazarh@gmail.com <Freed0m!20>
2018-04-26 15:09:01 +0300

    setting version to 0.1.0
    remove redundant requirements

--- package/setup.py
+++ package/setup.py
@@ -4,9 +4,6 @@ import os
 with open(os.path.join('version.txt')) as version_file:
     version_from_file = version_file.read().strip()
 
-with open('requirements.txt') as f_required:
-    required = f_required.read().splitlines()
-
 with open('test_requirements.txt') as f_tests:
     required_for_tests = f_tests.read().splitlines()
 
@@ -14,12 +11,12 @@ setup(
         name="cloudshell-cp-core",
         author="Quali",
         author_email="support@qualisystems.com",
-        description=("A repository for projects providing out of the box capabilities within CloudShell to parse cloushell requests bla "
-                     "apps in CloudShell sandboxes."),
+        description=("A repository for projects providing out of the box capabilities within CloudShell to parse "
+                     "cloushell action requests for apps in CloudShell sandboxes."),
         packages=find_packages(),
         test_suite='nose.collector',
         package_data={'': ['*.txt']},
-        install_requires=required,
+        install_requires=[],
         version=version_from_file,
         include_package_data=True,
         keywords="sandbox cloudshell json request",
@@ -27,6 +24,6 @@ setup(
             "Development Status :: 4 - Beta",
             "Topic :: Software Development :: Libraries",
             "License :: OSI Approved :: Apache Software License",
-        ], requires=['jsonpickle']
-
+        ],
+        requires=[]
 )
