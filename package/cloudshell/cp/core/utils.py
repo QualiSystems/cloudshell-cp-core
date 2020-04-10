c72debea2a3286e8a0d454518a3f36d08c766155
Anthony Poddubny <anton.p@qualisystems.com>
2020-04-10 20:24:16 +0300

    add generate_ssh_key_pair command

--- package/cloudshell/cp/core/utils.py
+++ package/cloudshell/cp/core/utils.py
@@ -1,4 +1,9 @@
-from cloudshell.cp.core.models import DriverResponseRoot,DriverResponse,Attribute
+from io import StringIO
+
+import paramiko
+
+from cloudshell.cp.core.models import DriverResponseRoot, DriverResponse, Attribute
+
 
 def set_value(target, name, value, raise_exeception = False):
 
@@ -75,3 +80,18 @@ def convert_dict_to_attributes_list(attributes_dict):
         attributes.append(Attribute(k,v))
 
     return attributes
+
+
+def generate_ssh_key_pair(bits=2048):
+    """Generate SSH key pair
+
+    :param int bits:
+    :rtype: tuple[str, str]
+    """
+    key = paramiko.RSAKey.generate(bits)
+    public_key = f"{key.get_name()} {key.get_base64()}"
+    private_key = StringIO()
+    key.write_private_key(private_key)
+    private_key.seek(0)
+
+    return private_key.read(), public_key
