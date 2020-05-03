f2185e4deb74f7e5c50f6d48adabb859fc2922c4
Anthony Poddubny <anton.p@qualisystems.com>
2020-05-04 01:29:13 +0300

    refactor core models

---
+++ package/cloudshell/cp/core/models/connectivity.py
@@ -0,0 +1,61 @@
+from dataclasses import dataclass, field
+
+from cloudshell.cp.core.models.base import BaseRequestObject, BaseRequestAction
+
+
+@dataclass
+class SetVlanParameter(BaseRequestObject):
+    vlanId: str = ""
+    mode: int = 0
+    vlanServiceAttributes: dict = field(default_factory=dict)
+
+
+@dataclass
+class ActionTarget(BaseRequestObject):
+    fullAddress: str = ""
+    fullName: str = ""
+
+
+@dataclass
+class BaseConnectivityAction(BaseRequestAction):
+    actionTarget: ActionTarget = None
+
+
+@dataclass
+class BaseConnectivityVlanAction(BaseConnectivityAction):
+    connectionId: str = ""
+    connectionParams: SetVlanParameter = None
+    connectorAttributes: dict = field(default_factory=dict)
+    customActionAttributes: dict = field(default_factory=dict)
+
+
+@dataclass
+class CleanupNetwork(BaseConnectivityAction):
+    pass
+
+
+@dataclass
+class RemoveVlan(BaseConnectivityVlanAction):
+    pass
+
+
+@dataclass
+class SetVlan(BaseConnectivityVlanAction):
+    pass
+
+
+@dataclass
+class ConnectToSubnetParams(BaseRequestObject):
+    cidr: str = ""
+    subnetId: str = ""
+    isPublic: bool = True
+    subnetServiceAttributes: dict = field(default_factory=dict)
+    vnicName: str = ""
+
+
+@dataclass
+class ConnectSubnet(BaseConnectivityAction):
+    actionParams: ConnectToSubnetParams = None
+
+    def is_private(self):
+        return self.actionParams.subnetServiceAttributes.get("Public", True) is False
