3d1e1c77610bfdc993e6c1daa8a1898535c262c7
Anthony Poddubny <anton.p@qualisystems.com>
2020-05-04 17:39:28 +0300

    add tox support

---
+++ cloudshell/cp/core/models/__init__.py
@@ -0,0 +1,10 @@
+from .action_results import *  # noqa
+from .base import *  # noqa
+from .connectivity import *  # noqa
+from .create_keys import *  # noqa
+from .deploy_app import *  # noqa
+from .driver_response import *  # noqa
+from .prepare_cloud_infra import *  # noqa
+from .prepare_subnet import *  # noqa
+from .save_restore_app import *  # noqa
+from .traffic_mirroring import *  # noqa
