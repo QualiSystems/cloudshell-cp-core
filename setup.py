a7f7ca1c991709dcc6f545764dc4e9f4c8e61065
Anthony Poddubny <anton.p@qualisystems.com>
2020-05-26 20:27:58 +0300

    refactor setup.py file

--- setup.py
+++ setup.py
@@ -1,28 +1,32 @@
+import os
+
 from setuptools import find_packages, setup
 
+with open(os.path.join("version.txt")) as version_file:
+    version_from_file = version_file.read().strip()
 
-def get_file_content(file_name):
-    with open(file_name) as f:
-        return f.read()
+with open("requirements.txt") as f_required:
+    required = f_required.read().splitlines()
 
+with open("test_requirements.txt") as f_tests:
+    required_for_tests = f_tests.read().splitlines()
 
 setup(
     name="cloudshell-cp-core",
-    url="http://www.qualisystems.com",
+    url="http://www.qualisystems.com/",
     author="Quali",
-    author_email="support@qualisystems.com",
+    author_email="info@quali.com",
+    packages=find_packages(),
+    install_requires=required,
+    python_requires="~=3.7",
+    tests_require=required_for_tests,
+    test_suite="nose.collector",
+    version=version_from_file,
     description=(
         "A repository for projects providing out of the box capabilities within CloudShell to parse and "
         "convert cloushell driver request to well defined python objects."
         "One cloudshell-cp-core For All cloudshell cloud provider shells."
     ),
-    long_description=get_file_content("README.md"),
-    tests_require=get_file_content("test_requirements.txt"),
-    test_suite="nose.collector",
-    packages=find_packages(),
-    include_package_data=True,
-    python_requires="~=3.7",
-    install_requires=get_file_content("requirements.txt"),
-    version=get_file_content("version.txt"),
     keywords="sandbox cloudshell json request",
+    include_package_data=True
 )
