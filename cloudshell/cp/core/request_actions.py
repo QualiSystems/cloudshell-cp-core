6f756acffad9e014f2f47e00dd98b848e6e0cef0
Anthony Poddubny <anton.p@qualisystems.com>
2020-05-26 20:28:32 +0300

    add more request actions

--- cloudshell/cp/core/request_actions.py
+++ cloudshell/cp/core/request_actions.py
@@ -39,31 +39,31 @@ class BaseRequestActions:
         elif isinstance(data, dict):
             try:
                 class_name = cls._normalize_class_name(data.pop("type"))
-                parsed_kwargs = cls._parse(data=data, cs_api=cs_api)
-                parsed_cls = getattr(models, class_name)
-
-                if issubclass(parsed_cls, models.DeployApp):
-                    parsed_cls = cls.REGISTERED_DEPLOYMENT_PATH_MODELS.get(
-                        parsed_kwargs["actionParams"].deployment.deploymentPath,
-                        parsed_cls,
-                    )
-                    parsed_obj = parsed_cls(**parsed_kwargs)
-                    parsed_obj.set_cloudshell_api(api=cs_api)
-                else:
-                    parsed_obj = parsed_cls(**parsed_kwargs)
-
-                return parsed_obj
-
             except KeyError:
                 parsed_params = {}
-
                 for param_key, param in data.items():
                     parsed_params[param_key] = cls._parse(data=param, cs_api=cs_api)
 
                 return parsed_params
 
-        if data.lower() in ("true", "false"):
-            data = data.lower() == "true"
+            parsed_kwargs = cls._parse(data=data, cs_api=cs_api)
+            parsed_cls = getattr(models, class_name)
+
+            if issubclass(parsed_cls, models.DeployApp):
+                parsed_cls = cls.REGISTERED_DEPLOYMENT_PATH_MODELS.get(
+                    parsed_kwargs["actionParams"].deployment.deploymentPath,
+                    parsed_cls,
+                )
+                parsed_obj = parsed_cls(**parsed_kwargs)
+                parsed_obj.set_cloudshell_api(api=cs_api)
+            else:
+                parsed_obj = parsed_cls(**parsed_kwargs)
+
+            return parsed_obj
+
+        elif isinstance(data, str):
+            if data.lower() in ("true", "false"):
+                data = data.lower() == "true"
 
         return data
 
@@ -132,3 +132,140 @@ class DeployVMRequestActions(BaseRequestActions):
                 obj.connect_subnets.append(action)
 
         return obj
+
+
+@dataclass
+class VMDetails:
+    id: str
+    cloud_provider_id: str
+    uid: str
+    vm_custom_params: dict = field(default_factory=dict)
+
+    @classmethod
+    def from_dict(cls, data):
+        """
+
+        :param data:
+        :return:
+        """
+        return cls(id=data["id"],
+                   cloud_provider_id=data["cloudProviderId"],
+                   uid=data["uid"],
+                   vm_custom_params=data["vmCustomParams"])
+
+
+@dataclass
+class DeployedApp:
+    PUBLIC_IP_KEY = "Public IP"
+
+    name: str
+    deployment_service_model: str
+    private_ip: str
+    attributes: dict = field(default_factory=dict)
+    vmdetails: VMDetails = None
+
+    @property
+    def user(self):
+        return self.attributes["User"]
+
+    @property
+    def password(self):
+        # todo: provide API and decode!!!!!
+        return self.attributes["Password"]
+
+    @property
+    def public_ip(self):
+        return self.attributes[self.PUBLIC_IP_KEY]
+
+    @classmethod
+    def from_data(cls, app_request_data, deployed_app_data):
+        """
+
+        :param dict app_request_data:
+        :param dict deployed_app_data:
+        :return:
+        """
+        attributes = {attr["name"]: attr["value"]
+                      for attr in deployed_app_data["attributes"]}
+
+        return cls(name=deployed_app_data["name"],
+                   deployment_service_model=app_request_data["deploymentService"]["model"],
+                   private_ip=deployed_app_data["address"],
+                   attributes=attributes,
+                   vmdetails=VMDetails.from_dict(deployed_app_data["vmdetails"]))
+
+    @classmethod
+    def from_remote_resource(cls, resource):
+        """
+
+        :param resource:
+        :return:
+        """
+        return cls.from_data(app_request_data=json.loads(resource.app_context.app_request_json),
+                             deployed_app_data=json.loads(resource.app_context.deployed_app_json))
+
+
+@dataclass
+class GetVMDetailsRequestActions:
+    deployed_apps: list = field(default_factory=list)
+
+    @classmethod
+    def from_request(cls, request):
+        data = json.loads(request)
+        deployed_apps = []
+        for item in data["items"]:
+            deployed_app = DeployedApp.from_data(app_request_data=item["appRequestJson"],
+                                                 deployed_app_data=item["deployedAppJson"])
+            deployed_apps.append(deployed_app)
+
+        return cls(deployed_apps=deployed_apps)
+
+
+@dataclass
+class CleanupSandboxInfraRequestActions(BaseRequestActions):
+    cleanup_network: models.CleanupNetwork = None
+
+    @classmethod
+    def from_request(cls, request, cs_api=None):
+        request = json.loads(request)
+        actions = cls._parse(data=request["driverRequest"].get("actions", []), cs_api=cs_api)
+        obj = cls()
+
+        for action in actions:
+            if isinstance(action, models.CleanupNetwork):
+                obj.cleanup_network = action
+
+        return obj
+
+
+@dataclass
+class SetAppSecurityGroupsRequestActions(BaseRequestActions):
+    @classmethod
+    def from_request(cls, request, cs_api=None):
+        request = json.loads(request)
+        actions = cls._parse(data=request["driverRequest"].get("actions", []), cs_api=cs_api)
+        obj = cls()
+        cls.actions = actions
+        #
+        # for action in actions:
+        #     if isinstance(action, models.CleanupNetwork):
+        #         obj.cleanup_network = action
+
+        return obj
+
+        security_group_models = []
+
+        security_groups = AzureModelsParser.get_app_security_groups_from_request(request)
+
+        for security_group in security_groups:
+            security_group_model = AppSecurityGroupModel()
+            security_group_model.deployed_app = DeployedApp()
+            security_group_model.deployed_app.name = security_group.deployedApp.name
+            security_group_model.deployed_app.vm_details = VmDetails()
+            security_group_model.deployed_app.vm_details.uid = security_group.deployedApp.vmdetails.uid
+            security_group_model.security_group_configurations = SecurityGroupParser.parse_security_group_configurations(
+                security_group.securityGroupsConfigurations)
+
+            security_group_models.append(security_group_model)
+
+        return security_group_models
