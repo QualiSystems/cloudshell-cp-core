97e59d99c1fbceedbf5bbad0ab3637c46b59d6dd
Anthony Poddubny <anton.p@qualisystems.com>
2020-06-02 21:21:28 +0300

    fix DriverResponse class

--- cloudshell/cp/core/request_actions/models/prepare_subnet.py
+++ cloudshell/cp/core/request_actions/models/prepare_subnet.py
@@ -17,12 +17,8 @@ class PrepareSubnet(BaseConnectivityAction):
     actionParams: PrepareSubnetParams = None
     attributes: dict = field(default_factory=dict)
 
-    def __post_init__(self):
-        for attr in self.actionParams.subnetServiceAttributes:
-            self.attributes[attr.attributeName] = attr.attributeValue
-
     def get_cidr(self):
         return self.actionParams.cidr
 
     def is_private(self):
-        return self.attributes.get("Public", True) is False
+        return not self.actionParams.isPublic
