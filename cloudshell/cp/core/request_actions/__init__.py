81c1ff856ad02c43c6830eec08d3d3495767582a
Anthony Poddubny <anton.p@qualisystems.com>
2020-06-05 15:35:04 +0300

    rework DeployApp models

--- cloudshell/cp/core/request_actions/__init__.py
+++ cloudshell/cp/core/request_actions/__init__.py
@@ -1,6 +1,6 @@
 from .cleanup_sandbox_infra import CleanupSandboxInfraRequestActions  # noqa
 from .deploy_vm import DeployVMRequestActions  # noqa
-from .deployed_app import DeployedApp  # noqa
+from .deployed_vm import DeployedVMRequestActions  # noqa
 from .driver_response import DriverResponse  # noqa
 from .get_vm_details import GetVMDetailsRequestActions  # noqa
 from .prepare_sandbox_infra import PrepareSandboxInfraRequestActions  # noqa
--- cloudshell/cp/core/request_actions/models/__init__.py
+++ cloudshell/cp/core/request_actions/models/__init__.py
@@ -3,6 +3,7 @@ from .base import *  # noqa
 from .connectivity import *  # noqa
 from .create_keys import *  # noqa
 from .deploy_app import *  # noqa
+from .deployed_app import *  # noqa
 from .prepare_cloud_infra import *  # noqa
 from .prepare_subnet import *  # noqa
 from .save_restore_app import *  # noqa
