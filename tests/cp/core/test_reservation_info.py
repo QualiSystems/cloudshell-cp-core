845c92c713a42839f876223e7cd1881663a552ac
Kyrylo Maksymenko <saklar13@gmail.com>
2021-05-05 11:47:27 +0300

    add tests

--- tests/cp/core/test_reservation_info.py
+++ tests/cp/core/test_reservation_info.py
@@ -1,24 +1,20 @@
-import unittest
-from unittest import mock
+from unittest.mock import MagicMock
+
+import pytest
 
 from cloudshell.cp.core.reservation_info import ReservationInfo
 
 
-class TestReservationInfo(unittest.TestCase):
-    def setUp(self):
-        self.context = mock.MagicMock()
+@pytest.fixture()
+def context():
+    return MagicMock()
 
-    def test_from_resource_context(self):
-        reservation_info = ReservationInfo.from_resource_context(context=self.context)
-        self.assertEqual(
-            reservation_info.reservation_id, self.context.reservation.reservation_id
-        )
 
-    def test_from_remote_resource_context(self):
-        reservation_info = ReservationInfo.from_remote_resource_context(
-            context=self.context
-        )
-        self.assertEqual(
-            reservation_info.reservation_id,
-            self.context.remote_reservation.reservation_id,
-        )
+def test_from_resource_context(context):
+    reservation_info = ReservationInfo.from_resource_context(context)
+    assert reservation_info.reservation_id == context.reservation.reservation_id
+
+
+def test_from_remote_resource_context(context):
+    reservation_info = ReservationInfo.from_remote_resource_context(context)
+    assert reservation_info.reservation_id == context.remote_reservation.reservation_id
