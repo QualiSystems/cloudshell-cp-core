4509f2ce648e8307754e69a89bfb6e6eae6c24fc
Kyrylo Maksymenko <saklar13@gmail.com>
2022-01-19 17:12:01 +0200

    add test for Generic Deployed App

--- tests/cp/core/request_actions/test_deployed_vm.py
+++ tests/cp/core/request_actions/test_deployed_vm.py
@@ -11,6 +11,10 @@ class StaticApp(DeployedApp):
     DEPLOYMENT_PATH = "Generic Static vCenter VM 2G"
 
 
+class MyDeployedApp(DeployedApp):
+    DEPLOYMENT_PATH = "MicrosoftAzure2G.AzureVMFromMarketplace2G"
+
+
 def test_static_deployed_app():
     app_name = "win-test"
     address = "192.168.26.43"
@@ -26,16 +30,16 @@ def test_static_deployed_app():
                 "address": address,
                 "attributes": [
                     {
-                        "name": "Generic Static vCenter VM 2G.VM Name",
+                        "name": f"{StaticApp.DEPLOYMENT_PATH}.VM Name",
                         "value": "static-vms/win-test",
                     },
                     {
-                        "name": "Generic Static vCenter VM 2G.vCenter Resource Name",
+                        "name": f"{StaticApp.DEPLOYMENT_PATH}.vCenter Resource Name",
                         "value": "vcenter",
                     },
-                    {"name": "Generic Static vCenter VM 2G.User", "value": ""},
-                    {"name": "Generic Static vCenter VM 2G.Password", "value": ""},
-                    {"name": "Generic Static vCenter VM 2G.Public IP", "value": ""},
+                    {"name": f"{StaticApp.DEPLOYMENT_PATH}.User", "value": ""},
+                    {"name": f"{StaticApp.DEPLOYMENT_PATH}.Password", "value": ""},
+                    {"name": f"{StaticApp.DEPLOYMENT_PATH}.Public IP", "value": ""},
                     {"name": "Execution Server Selector", "value": ""},
                 ],
                 "vmdetails": {
@@ -80,3 +84,93 @@ def test_static_deployed_app():
     assert app.private_ip == address
     assert app.vmdetails.uid == uuid
     assert app.attributes[f"{StaticApp.DEPLOYMENT_PATH}.VM Name"] == vm_name
+
+
+def test_deployed_app():
+    app_name = "Azureubuntusimple"
+    address = "10.0.1.3"
+    deployed_model = "Generic App Model"
+    uuid = "3d750874-09f1-4243-a598-6700bb648655"
+
+    app_context = AppContext(
+        app_request_json=json.dumps(
+            {
+                "name": app_name,
+                "description": None,
+                "logicalResource": {
+                    "family": "GenericAppFamily",
+                    "model": "GenericAppModel",
+                    "driver": None,
+                    "description": "",
+                    "attributes": [
+                        {"name": "Password", "value": ""},
+                        {"name": "PublicIP", "value": ""},
+                        {"name": "User", "value": ""},
+                    ],
+                },
+                "deploymentService": {
+                    "cloudProviderName": "Azure2Goldcode",
+                    "name": "Microsoft Azure 2nd Gen.Azure VM From Marketplace 2nd Gen",
+                    "model": MyDeployedApp.DEPLOYMENT_PATH,
+                    "driver": None,
+                    "attributes": [
+                        {
+                            "name": "MicrosoftAzure2G.AzureVMFromMarketplace2G.Disk",
+                            "value": "HDD",
+                        },
+                    ],
+                },
+            }
+        ),
+        deployed_app_json=json.dumps(
+            {
+                "name": app_name,
+                "family": "Generic App Family",
+                "model": "Generic App Model",
+                "address": address,
+                "attributes": [
+                    {"name": "Password", "value": "3M3u7nkDzxWb0aJ/IZYeWw=="},
+                    {"name": "User", "value": "adminuser"},
+                    {"name": "Public IP", "value": ""},
+                ],
+                "vmdetails": {
+                    "id": "4da74d28-50d9-4271-b2e4-b49eed1bb0fe",
+                    "cloudProviderId": "129b8fac-fd8d-4c37-bb1a-fdceba2f38d7",
+                    "uid": uuid,
+                    "vmCustomParams": [],
+                },
+            }
+        ),
+    )
+    resource = ResourceContextDetails(
+        id="0917eb75-92ad-4291-9623-4235c81be76b",
+        name=app_name,
+        fullname=app_name,
+        type="Resource",
+        address=address,
+        model="Generic App Model",
+        family="Generic App Family",
+        description=None,
+        attributes={
+            "Password": "3M3u7nkDzxWb0aJ/IZYeWw==",
+            "User": "adminuser",
+            "Public IP": "",
+        },
+        app_context=app_context,
+        networks_info=None,
+        shell_standard=None,
+        shell_standard_version=None,
+    )
+
+    DeployedVMActions.register_deployment_path(MyDeployedApp)
+    actions = DeployedVMActions.from_remote_resource(resource, Mock())
+
+    app = actions.deployed_app
+    assert isinstance(app, MyDeployedApp)
+    assert app.name == app_name
+    assert app.model == deployed_model
+    assert app.deployment_service_model == MyDeployedApp.DEPLOYMENT_PATH
+    assert app.private_ip == address
+    assert app.vmdetails.uid == uuid
+    assert app.attributes[f"{MyDeployedApp.DEPLOYMENT_PATH}.Disk"] == "HDD"
+    assert app.attributes["User"] == "adminuser"
