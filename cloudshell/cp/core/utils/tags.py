93a8555cbc8cc763bc5415e8231fc9d6aec7e16a
Kyrylo Maksymenko <saklar13@gmail.com>
2022-02-22 13:58:03 +0200

    set blueprint name to "CloudShell Saved Sandbox" if sandbox creates from saved sandbox

--- cloudshell/cp/core/utils/tags.py
+++ cloudshell/cp/core/utils/tags.py
@@ -8,6 +8,7 @@ class BaseTagsManager:
 
     class DefaultTagValues:
         created_by = "CloudShell"
+        blueprint_name_from_saved_reservation = "CloudShell Saved Sandbox"
 
     def __init__(self, reservation_info, resource_config):
         self._reservation_info = reservation_info
@@ -15,10 +16,15 @@ class BaseTagsManager:
 
     def get_default_tags(self):
         """Get pre-defined CloudShell tags."""
+        bp_name = self._reservation_info.blueprint
+        if not bp_name:
+            # when restoring from saved sandbox blueprint name is empty
+            bp_name = self.DefaultTagValues.blueprint_name_from_saved_reservation
+
         return {
             self.DefaultTagNames.created_by: self.DefaultTagValues.created_by,
             self.DefaultTagNames.owner: self._reservation_info.owner,
-            self.DefaultTagNames.blueprint: self._reservation_info.blueprint,
+            self.DefaultTagNames.blueprint: bp_name,
             self.DefaultTagNames.sandbox_id: self._reservation_info.reservation_id,
             self.DefaultTagNames.domain: self._reservation_info.domain,
         }
