d2d6349211816378488021aa701f6f65268afbe7
Kyrylo Maksymenko <saklar13@gmail.com>
2022-06-16 17:50:13 +0200

    add prefix length param, move editing app name to a method

--- cloudshell/cp/core/utils/name_generator.py
+++ cloudshell/cp/core/utils/name_generator.py
@@ -6,6 +6,7 @@ import uuid
 import attr
 
 CS_MAX_NAME_LENGTH = 100
+CS_PATTER_REMOVE_SYMBOLS = re.compile(re.escape(".-|_[]"))
 
 
 def generate_name(name: str, postfix: str | None = None, max_length: int = 24) -> str:
@@ -38,8 +39,9 @@ class NameGenerator:
     Will replace all special characters.
     """
 
-    pattern_remove_symbols: re.Pattern = re.compile(re.escape(".-|_[]"))
+    pattern_remove_symbols: re.Pattern = CS_PATTER_REMOVE_SYMBOLS
     max_length: int = CS_MAX_NAME_LENGTH
+    prefix_length: int = 8
 
     def __call__(self, app_name: str, postfix: str | None = None) -> str:
         if postfix is None:
@@ -49,20 +51,24 @@ class NameGenerator:
                 f"Postfix '{postfix}' is too big. Max name length is {self.max_length}"
             )
 
-        app_name = self.pattern_remove_symbols.sub("", app_name)
-        app_name = app_name[: self.max_length - len(postfix) - 1].rstrip("-")
+        app_name = self._edit_app_name(app_name)
+
+        max_length = min(self.max_length, CS_MAX_NAME_LENGTH)
+        app_name = app_name[: max_length - len(postfix) - 1].rstrip("-")
 
         return f"{app_name}-{postfix}"
 
-    @staticmethod
-    def _generate_postfix() -> str:
-        return generate_short_unique_string()
+    def _generate_postfix(self) -> str:
+        return generate_short_unique_string(self.prefix_length)
+
+    def _edit_app_name(self, app_name: str) -> str:
+        return self.pattern_remove_symbols.sub("", app_name)
 
 
-def generate_short_unique_string() -> str:
+def generate_short_unique_string(length: int = 8) -> str:
     """Generate a short unique string.
 
     Method generates an uuid and return the first 8 characters of the new uuid
     """
-    unique_id = str(uuid.uuid4())[:8]
+    unique_id = str(uuid.uuid4()).replace("-", "")[:length]
     return unique_id
