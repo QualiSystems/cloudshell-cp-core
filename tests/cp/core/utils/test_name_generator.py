cab36bca2137a49766d8c9363ce84ee2d4ca918d
Kyrylo Maksymenko <saklar13@gmail.com>
2022-06-17 14:42:28 +0200

    fix removing symbols

--- tests/cp/core/utils/test_name_generator.py
+++ tests/cp/core/utils/test_name_generator.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 
 from cloudshell.cp.core.utils.name_generator import NameGenerator
@@ -15,3 +17,11 @@ def test_name_generator_postfix_is_too_long():
     generate_name = NameGenerator(max_length=10)
     with pytest.raises(ValueError):
         generate_name("test", "postfix-too-long")
+
+
+def test_symbols_removed():
+    generate_name = NameGenerator()
+    name = generate_name(r"name test-123.-|_[]\/!@#$%^&*()")
+    name, postfix = name.rsplit("-", 1)
+    assert name == "name test-123.-|_[]"
+    assert re.search(r"[\d\w]{8}", postfix)
